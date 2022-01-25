07bdb74b10c11ec42af936b07308588e67ff2825
dbsgur <kkr431@naver.com>
2022-01-25 14:27:48 +0900

    1/25 CRUD board - Signup, Login

--- Januray/border/app.py
+++ Januray/border/app.py
@@ -17,9 +17,36 @@ def home():
 def board():
     return render_template('board.html')
 
+# sing up
+@app.route('/user/signup', methods=['POST'])
+def signup():
+    # id, password 받아오고 저장
+    user_id = request.form['user_id']
+    user_pwd = request.form['user_pwd']
+    # id 중복확인
+    if db.board.count_documents({'user_id':user_id}) == 0 :
+        db.board.insert_one({'user_id':user_id, 'user_pwd': user_pwd})
+        return jsonify({'result':'success', 'message':'SIGN UP SUCCESS'})
+    else :
+        return jsonify({'result': 'FAIL', 'message':'user_id already exists'})
+
+# log in
 @app.route('/user/login', methods=['POST'])
 def login():
-    return 'sss'
+    # id, password 받아오귀
+    user_id = request.form['user_id']
+    user_pwd = request.form['user_pwd']
+    # id 확인
+    if db.board.count_documents({'user_id': user_id}) == 0:
+        return jsonify({'result':'FAIL', 'message': 'WRONG ID'})
+    else :
+        # 비번 확인
+        # count_documents VS findone?
+        if db.board.count_documents({'user_id':user_id, 'user_pwd':user_pwd}) == 0 :
+            return jsonify({'result':'FAIL', 'message': 'WRONG PWD'})
+        else:
+            return jsonify({'result':'SUCCESS', 'message': 'LOGIN SUCCESS'})
+
 
 
 
--- Januray/border/static/js/app.js
+++ Januray/border/static/js/app.js
@@ -1,1 +1,50 @@
-console.log("SRC CONNECT");
+//회원가입
+function onClickSignUp(e) {
+  e.preventDefault();
+  // html에서 user_id user_pwd 가져오기
+  let user_id = $("#inputID").val();
+  let user_pwd = $("#inputPassword").val();
+  let confirm_pwd = $("#inputPasswordConfirm").val();
+  // 비밀번호 확인
+  if (user_pwd !== confirm_pwd) {
+    alert("비밀번호가 일치하지않습니다.");
+  } else {
+    $.ajax({
+      type: "POST",
+      url: "/user/signup",
+      //user_id , user_pwd
+      data: { user_id: user_id, user_pwd: user_pwd },
+      success: function (response) {
+        console.log(response);
+        if (response["result"] == "success") {
+          // 2. '좋아요 완료!' 얼럿을 띄웁니다.
+          alert(response["message"]);
+          // 3. 변경된 정보를 반영하기 위해 새로고침합니다.
+          window.location.reload();
+        } else {
+          alert(response["message"]);
+        }
+      },
+    });
+  }
+}
+
+function onClickLogIn(e) {
+  e.preventDefault();
+  let user_id = $("#loginId").val();
+  let user_pwd = $("#loginPassword").val();
+  console.log(user_id, user_pwd);
+  $.ajax({
+    type: "POST",
+    url: "/user/login",
+    //user_id , user_pwd
+    data: { user_id: user_id, user_pwd: user_pwd },
+    success: function (response) {
+      console.log(response);
+      alert(response["message"]);
+      if (response["result"] == "success") {
+        window.location.reload();
+      }
+    },
+  });
+}
