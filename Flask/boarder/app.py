b15b3b22b7f916351c4fb26a195a2f8f4cfd4caa
dbsgur <kkr431@naver.com>
2022-01-27 11:34:07 +0900

    1/26 flask-bcrypt

--- Flask/boarder/app.py
+++ Flask/boarder/app.py
@@ -1,11 +1,11 @@
 from pymongo import MongoClient
 from flask import Flask, render_template, jsonify, request
-#  pip install flask-bcrypt
+from flask_bcrypt import Bcrypt, generate_password_hash, check_password_hash
 # from flask.ext.bcrypt import Bcrypt
-
+#  pip install flask-bcrypt
 
 app = Flask(__name__)
-# bcrypt = Bcrypt(app)
+bcrypt = Bcrypt(app)
 client = MongoClient('localhost', 27017)
 db = client.dbborder
 
@@ -27,9 +27,10 @@ def signup():
     # id, password 받아오고 저장
     user_id = request.form['user_id']
     user_pwd = request.form['user_pwd']
+    pw_hash = generate_password_hash(user_pwd, 10)
     # id 중복확인
     if db.board.count_documents({'user_id': user_id}) == 0:
-        db.board.insert_one({'user_id': user_id, 'user_pwd': user_pwd})
+        db.board.insert_one({'user_id': user_id, 'user_pwd': pw_hash})
         return jsonify({'result': 'SUCCESS', 'message': 'SIGN UP SUCCESS'})
     else:
         return jsonify({'result': 'FAIL', 'message': 'user_id already exists'})
@@ -48,10 +49,15 @@ def login():
     else:
         # 비번 확인
         # count_documents VS findone?
-        if db.board.count_documents({'user_id': user_id, 'user_pwd': user_pwd}) == 0:
-            return jsonify({'result': 'FAIL', 'message': 'WRONG PWD'})
-        else:
+        check_pwd = db.board.find_one({"user_id":user_id})
+        if check_password_hash(check_pwd.get('user_pwd'), user_pwd):
             return jsonify({'result': 'SUCCESS', 'message': 'LOGIN SUCCESS'})
+        else :
+            return jsonify({'result': 'FAIL', 'message': 'WRONG PWD'})
+        # if db.board.count_documents({'user_id': user_id, 'user_pwd': user_pwd}) == 0:
+        #     return jsonify({'result': 'FAIL', 'message': 'WRONG PWD'})
+        # else:
+        #     return jsonify({'result': 'SUCCESS', 'message': 'LOGIN SUCCESS'})
 
 
 if __name__ == '__main__':
