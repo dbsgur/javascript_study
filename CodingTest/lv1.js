3c5b2dd7f864f59e9950b911ea51150e45bdbf9b
dbsgur <kkr431@naver.com>
2022-02-01 17:19:14 +0900

    2/1 Javascript programmers codingtest lv1 1문제....ㅜ

--- CodingTest/lv1.js
+++ CodingTest/lv1.js
@@ -997,3 +997,34 @@ function solution(board, moves) {
   }
   return answer;
 }
+
+//
+
+//숫자 문자열과 영단어
+function solution(s) {
+  var answer = 0;
+  s=s.replace(/zero/gi,0)
+  s=s.replace(/one/g,1)
+  s=s.replace(/two/g, 2)
+  s=s.replace(/three/g,3)
+  s=s.replace(/four/g,4)
+  s=s.replace(/five/g,5)
+  s=s.replace(/six/g,6)
+  s=s.replace(/seven/g,7)
+  s=s.replace(/eight/g,8)
+  s=s.replace(/nine/g,9)
+  
+  return +s;
+}
+
+function solution(s) {
+  let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+  var answer = s;
+
+  for(let i=0; i< numbers.length; i++) {
+      let arr = answer.split(numbers[i]);
+      answer = arr.join(i);
+  }
+
+  return Number(answer);
+}
