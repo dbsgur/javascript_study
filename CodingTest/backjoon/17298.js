5e07af0f0f38314f33b32b495f00ca24568a3023
dbsgur <kkr431@naver.com>
2022-02-12 16:07:02 +0900

    2/12 백준 1798 + 정글 면접ㅜㅜ 떨어지면 뭐할꺼냐 ? 불합격 플래그인가...

---
+++ CodingTest/backjoon/17298.js
@@ -0,0 +1,22 @@
+var fs = require("fs");
+var input = fs
+  .readFileSync(__dirname + "/input.txt")
+  .toString()
+  .split("\n");
+var cases = input[0];
+var splited = input[1].split(" ");
+var stack = [];
+for (var i = 0; i < Number(cases); i++) {
+  while (
+    stack.length !== 0 &&
+    Number(splited[i]) > Number(splited[stack[stack.length - 1]])
+  ) {
+    splited[stack.pop()] = splited[i];
+  }
+  stack.push(i);
+}
+
+while (stack.length !== 0) {
+  splited[stack.pop()] = -1;
+}
+console.log(splited.join(" ").trim());
